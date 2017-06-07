4f8735ce441b2d48981024f9872021d81756f4e4
Romain <seite.romain@yahoo.com>
2017-06-07 15:00:44 +0200

    Add boucle

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  */ ?>
 <?php get_header(); ?>
 
+
 <div class="container">
     <header>
         <!-- Début du menu-->
@@ -24,7 +25,7 @@
         <!-- Titres header-->
         <h1 class="titre-header"><?php bloginfo('name'); ?></h1>
         <h2 class="soustitre-header">
-            <?php bloginfo('description')?>
+            <?php bloginfo('description') ?>
         </h2>
     </header>
     <!-- // Fin du header-->
@@ -44,52 +45,26 @@
     <!-- Début du jumbetron-->
     <div class="jumbetron">
         <p class="headline">
-            Make first impression count with a large headline & full-page background.<br> Create is super easy to use, works out-of-the-box and it's great for your personal, business or even <br> organization site.
+            Make first impression count with a large headline & full-page background.<br> Create is super easy to use,
+            works out-of-the-box and it's great for your personal, business or even <br> organization site.
         </p>
     </div>
     <!-- // Fin du jumbetron-->
 
     <!-- Barre de séparation-->
     <div class="separation"></div>
+    <div id="content">
+        <?php if (have_posts()){
+            while (have_posts()){
+                the_post();
+                the_title();
+                the_content();
+            }
+        }
 
-    <!-- Début de la section articles-->
-    <section id="articles">
-        <article>
-            <img class="article-img" src="images/img1.jpg" alt="image article">
-            <h3 class="article-titre">Catch flames pro</h3>
-            <div class="article-content">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta. Donec id elit neque.
-                    Sed fringilla justo ut metus sodales bibendum. Quisque aliquet tristique est sed dapibus. Ut sed sagittis tellus, sed sagittis turpis. Cras pharetra imperdiet nisl id vulputate. Aliquam non purus eget felis vulputate auctor quis
-                    nec ante. Cras laoreet nibh id lorem pellentesque sollicitudin. Nunc nunc magna, fringilla a eleifend et, sodales nec leo. Nulla eu turpis ut ipsum condimentum imperdiet sit amet sit amet turpis. Sed posuere volutpat justo vitae
-                    ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam elementum egestas ornare. Sed suscipit magna. [...]</p>
-            </div>
-        </article>
+        ?>
+    </div>
 
-        <article>
-            <img class="article-img" src="images/img2.jpg" alt="image article">
-            <h3 class="article-titre">Adventurous pro</h3>
-            <div class="article-content">
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta.
-                    Donec id elit neque. Sed fringilla justo ut metus sodales bibendum. Quisque aliquet tristique est sed dapibus. Ut sed sagittis tellus, sed sagittis turpis. Cras pharetra imperdiet nisl id vulputate. Aliquam non purus eget felis
-                    vulputate auctor quis nec ante. Cras laoreet nibh id lorem pellentesque sollicitudin. Nunc nunc magna, fringilla a eleifend et, sodales nec leo. Nulla eu turpis ut ipsum condimentum imperdiet sit amet sit amet turpis. Sed posuere
-                    volutpat justo vitae ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam elementum egestas ornare. Sed suscipit magna. [...]</p>
-            </div>
-        </article>
-
-
-        <article>
-            <img class="article-img" src="images/img3.jpg" alt="image article">
-            <h3 class="article-titre">Catch kathmandu pro</h3>
-            <div class="article-content">
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis, urna non accumsan pellentesque, lacus justo consequat risus, nec fermentum sem nulla ac sapien. Maecenas pharetra urna et elit sodales, vitae vestibulum massa porta.
-                    Donec id elit neque. Sed fringilla justo ut metus sodales bibendum. Quisque aliquet tristique est sed dapibus. Ut sed sagittis tellus, sed sagittis turpis. Cras pharetra imperdiet nisl id vulputate. Aliquam non purus eget felis
-                    vulputate auctor quis nec ante. Cras laoreet nibh id lorem pellentesque sollicitudin. Nunc nunc magna, fringilla a eleifend et, sodales nec leo. Nulla eu turpis ut ipsum condimentum imperdiet sit amet sit amet turpis. Sed posuere
-                    volutpat justo vitae ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam elementum egestas ornare. Sed suscipit magna. [...]</p>
-            </div>
-        </article>
-    </section>
-    <!-- // Fin de la section articles-->
 </div>
 <!-- // fermeture du container -->
 </body>
